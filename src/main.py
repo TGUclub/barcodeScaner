bb48140590a6984ec98619dcf4905abba9ecdbc9
siuud <3563264307@qq.com>
2023-03-25 20:38:24 +0800

    添加了检测摄像头的功能

--- src/main.py
+++ src/main.py
@@ -1,16 +1,43 @@
 import cv2
+import os
+import pygame.camera
 from pyzbar import pyzbar
 from time import sleep
 
+pygame.camera.init()
+cameras = pygame.camera.list_cameras()
+avaliableCameras = []
+
+# check cameras
+for cam in cameras:
+    try:
+        pygame.camera.Camera(cam)
+        avaliableCameras.append(cam)
+    except pygame.error:
+        pass
+    finally:
+        os.system("cls")
+
+
+print(f"avaliable cameras list {avaliableCameras}")
+
+indexOfCamera = int(input("Enter the index of camera: "))
+
 # Initialize the camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(indexOfCamera)
+
+if cap is None:
+    print("Failed to open camere")
+
+
 interval = 1
 
 
 while True:
     # Capture a frame from the camera
-    # ret, frame = cap.read()
-    frame = cv2.imread('test.png')
+    ret, frame = cap.read()
+    if ret is False:
+        print("End of file")
 
     # Decode the barcode from the frame
     barcodes = pyzbar.decode(frame)
@@ -25,6 +52,7 @@ while True:
         print(f"Barcode data: {barcode_data}, Barcode type: {barcode_type}")
 
     # Display the frame
+
     cv2.imshow('Barcode Scanner', frame)
 
     # Check for key press
